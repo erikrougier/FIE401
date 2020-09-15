1cb40928fc431a4016e8e54b3bf6f5228badc321
Alehog <71270272+Alehog@users.noreply.github.com>
2020-09-15 23:48:46 +0200

    Add files via upload

--- Innlevering1.R
+++ Innlevering1.R
@@ -13,6 +13,7 @@ library(lmtest)
 library(moonBook)
 library(ggiraph)
 library(ggiraphExtra)
+library(formattable)
 
 #Set working directory
 setwd("/Users/adhog/OneDrive/Dokumenter/Fie401")
@@ -38,7 +39,62 @@ for(i in c(3,8:11, 14:17)){df[,i] <- Winsorize(df[,i], probs=c(0.005, 0.995), na
 #Creates table for yearly mean
 aggregate(df[, c(3, 5, 7:8)], list(df$yyyy), mean)
 
+df2 <- data.frame(distinct(df$yyyy))
+df2 <- aggregate(df[, c(3, 5, 7:8)], list(df$yyyy), mean)
 
+names(df2)[1] <- "Table1"
+names(df2)[2] <- "Mean.deal.value"
+names(df2)[3] <- "Share.of.private.firms"
+names(df2)[4] <- "Share.of.deals.all.stock"
+names(df2)[5] <- "Mean.CAR"
+
+df2[2:5] <- round(df2[2:5], digits=3)
+
+test <- formatter("span", style = x ~ style(color = ifelse(x > 0, "green", 
+                  ifelse(x < 0, "red", "black"))))
+
+formattable(df2, list(Mean.CAR = test))
+
+df3 <- aggregate(df[, c(3,5,8)], list(df$all_stock), mean)
+
+names(df3)[1] <- "all_stock"
+
+
+#Creates vector test to save the p-values for later
+test <- c()
+
+#Sets a base value for x to be used in lookup for the means
+x <- 2
+
+#Extracts the p-value of the different 
+for(i in c(3,5,8)){pval <- t.test(df[df$all_stock ==0, i],
+                                  mu = df3[df3$Group.1==0, x])$p.value 
+                                  test[x-1] <- pval
+                                  x <- x+1
+                                  
+}
+
+test2 <- c()
+x <- 2
+
+for(i in c(3,5,8)){pval <- t.test(df[df$all_stock ==1, i],
+                                  mu = df3[df3$Group.1==1, x])$p.value 
+test2[x-1] <- pval
+x <- x+1
+
+}
+
+all_stock <- c(0, 1)
+pval3 <- c(test[1], test2[1])
+pval5 <- c(test[2], test2[2])
+pval8 <- c(test[3], test2[3])
+
+append <- data.frame(all_stock, pval3, pval5, pval8)
+
+left_join(df3, append)
+
+
+pval3
 #Creates all regression models
 fit1 <- lm(carbidder ~ all_stock, data =df[df$public == 1,])
 fit2 <- lm(carbidder ~ all_stock + bidder_mtb + bidder_size + deal_value, data =df[df$public == 1,])
