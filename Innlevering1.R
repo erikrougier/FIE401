bd2e0ad9b42810d6b97d28a6f0d2c3cddc12f2f7
Alehog <71270272+Alehog@users.noreply.github.com>
2020-09-15 13:32:56 +0200

    Add files via upload

--- Innlevering1.R
+++ Innlevering1.R
@@ -1,3 +1,5 @@
+#For now we run every package used so far just as a safety measure
+
 library(data.table)
 library(bit64)
 library(tidyverse)
@@ -8,12 +10,21 @@ library(stargazer)
 library(DescTools)
 library(car)
 library(lmtest)
+library(moonBook)
+library(ggiraph)
+library(ggiraphExtra)
+
+#Set working directory
 setwd("/Users/adhog/OneDrive/Dokumenter/Fie401")
 
+#Load the data file
 load("CAR_M&A.RData")
 
+#Rename the data for easier references
 df <- CAR_MA
 
+
+#Change the date columns to date/chr format
 df$yyyymmdd <- as.character(df$yyyymmdd)
 df$yyyymmdd <- as.Date(df$yyyymmdd, format="%Y%m%d")
 
@@ -21,22 +32,30 @@ df$yyyy <- as.character(df$yyyy)
 df$yyyy <- format(as.Date(df$yyyy, format="%Y"),"%Y")
 
 
-for(i in c(3,8:11, 14:17)){df[,i] <- Winsorize(df[,i], c(0.005, 0.995), na.rm = T)}
+#Winzorising at the 1% lvl to exclude extreme values in all the columns that are not binary
+for(i in c(3,8:11, 14:17)){df[,i] <- Winsorize(df[,i], probs=c(0.005, 0.995), na.rm = T)}
 
+#Creates table for yearly mean
 aggregate(df[, c(3, 5, 7:8)], list(df$yyyy), mean)
 
 
-aggregate(df[, c(3, 5, 7:8)], list(df$all_stock), mean)
-
-
-
-
+#Creates all regression models
 fit1 <- lm(carbidder ~ all_stock, data =df[df$public == 1,])
-fit2 <- lm(carbidder ~ all_stock + 0,3*bidder_mtb, data =df[df$public == 1,])
+fit2 <- lm(carbidder ~ all_stock + bidder_mtb + bidder_size + deal_value, data =df[df$public == 1,])
 fit3 <- lm(carbidder ~ all_stock, data =df[df$public == 0,])
-fit4 <- lm(carbidder ~ all_stock + 0,3*bidder_mtb, data =df[df$public == 0,])
-fit5 <- lm(carbidder ~ all_stock, data = df)
-fit6 <- lm(carbidder ~ all_stock + 0,3*bidder_mtb, data =df)
+fit4 <- lm(carbidder ~ all_stock + bidder_mtb + bidder_size + deal_value, data =df[df$public == 0,])
+fit5 <- lm(carbidder ~ all_stock*public, data =df)
+fit6 <- lm(carbidder ~ all_stock*public + bidder_mtb + bidder_size + deal_value , data =df)
+
+#Test the hostile variable for last question
+fit7 <- lm(carbidder ~ hostile, data =df[df$public == 0,])
+
+stargazer(fit7, type="text")
+
+#Plot the fit 5
+ggPredict(fit5)
+
+#Creates table in html on the data from the different regression models
+stargazer(fit1,fit2,fit3,fit4,fit5,fit6, type="latex", report="vc*t", out="models.htm")
 
 
-stargazer(fit1,fit2,fit3,fit4,fit5,fit6, type="text", report="vc*t")
