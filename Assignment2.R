30f5f5aa419370f4ef2c318c049988a9abbec60a
erikrougier <62702738+erikrougier@users.noreply.github.com>
2020-10-06 23:36:49 +0200

    Update Assignment2.R

--- Assignment2.R
+++ Assignment2.R
@@ -13,6 +13,7 @@ library(ggiraph)
 library(ggiraphExtra)
 library(formattable)
 library(lubridate)
+library(pander)
 
 #Clear the environment
 rm(list=ls())
@@ -39,8 +40,9 @@ for(i in c(2:length(ESP_VW))){
 for(i in c(2:length(BMP_VW))){
   names(BMP_VW)[i] <- paste("BMP_", names(BMP_VW)[i], sep="")
 }
-dat <- left_join(ESP_VW, BMP_VW, by = "date")
-dat <- left_join(dat, PF, by = "date")
+#Using inner_join to merge column by date to avoid having missing data
+dat <- inner_join(ESP_VW, BMP_VW, by = "date")
+dat <- inner_join(dat, PF, by = "date")
 
 #Date columns need to be set in date format
 dat$date <- as.Date(as.character(dat$date), "%Y%m%d")
@@ -48,7 +50,36 @@ dat$date <- as.Date(as.character(dat$date), "%Y%m%d")
 #Remove obsolete dataframe
 rm(BMP_VW, ESP_VW, PF)
 
+#Extracting betas from regression coefficients of exposure of 10 ESP portfolios to LIQ
+beta_ESP <- c(as.numeric(lapply(2:11, 
+                                function(x) lm(dat[,x] ~ dat$LIQ)$coefficients[2])))
+
+#Making a vector of the average return
+mean_ESP <- c(as.numeric(lapply(2:11, 
+                                function(x) mean(dat[,x]))))
+
+#Storing the average return and exposure in a df
+exposure_vs_return <- data.frame(Exposure = beta_ESP,
+                      Mean = mean_ESP)
+
+#Creating a point for the beta of a given portfolio with the corresponding return 
+#Smoothed line to predict outcome of the portfolios between each datapoint
+ggplot(exposure_vs_return, 
+       aes(x = Exposure, y = Mean))+
+  labs(title= "Exposure vs Returns", x = "Exposure", y = "Mean Return")+
+  geom_smooth(color="lightgrey")+
+  geom_point(color="darkred")+
+  scale_y_continuous(labels = scales::percent)+
+  ggsave(filename = "Assigment2_plot1.jpg")
+
+#Create gammas
+# Making the gamma values from all the monthly retruns
+gammas <- c(as.numeric(lapply(1:length(dat[,1]), 
+                           function(x) lm(as.numeric(dat[x,2:11]) ~ beta_ESP)$coefficients[2])))
 
 
-
-  
+#Running a t-test on the sample mean and expected mean of 0. 
+test <- t.test(gammas, mu = 0)
+test$p.value
+pander(test)
+#Conclusion: mean gammas are significantly different from zero
